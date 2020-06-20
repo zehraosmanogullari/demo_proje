a12023870b1b2593cf554c7bce41b5fbe04a3573
zehraosmanogullari <67191906+zehraosmanogullari@users.noreply.github.com>
2020-06-20 22:13:35 +0200

    Update DemoClass.java
    commit remove reporsitory den"

--- src/demopackage/DemoClass.java
+++ src/demopackage/DemoClass.java
@@ -4,6 +4,7 @@ public class DemoClass {
 
 	public static void main(String[] args) {
 		System.out.println("ilk commit local reporsitory den");
+		System.out.println("ilk commit remote reporsitory den");
 
 	}
 
